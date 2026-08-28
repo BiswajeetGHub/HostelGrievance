28a5a6feddcfd303c440e68b082e328f10b1f800
Biswajeet Senapati <ayushsenapati78@gmail.com>
2026-08-28 21:58:29 +0530

    fix: remove node_modules, add gitignore

---
+++ .svelte-kit/generated/client/nodes/3.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/attempt-placeholder/+page.svelte";
---
+++ .svelte-kit/generated/client/nodes/6.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/student/grievances/+page.svelte";
